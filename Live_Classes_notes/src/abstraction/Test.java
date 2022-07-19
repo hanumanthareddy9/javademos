61acdaa2be97a5a2cac0d3187d03f63643f75c18
hanumanthareddy9 <hanumanthareddy973@gmail.com>
2022-07-19 12:54:23 +0530

    chages

--- Live_Classes_notes/src/abstraction/Test.java
+++ Live_Classes_notes/src/abstraction/Test.java
@@ -3,8 +3,6 @@ package abstraction;
 public class Test {
 
 	public static void main(String[] args) {
-//		BMW bmw = new BMW();
-		
 		BMW3Series threes = new BMW3Series();
 		threes.accelarate();
 		threes.commonFunctionality();
