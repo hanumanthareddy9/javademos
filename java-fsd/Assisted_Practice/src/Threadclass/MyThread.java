62c101962dbf445732a94d471bab46bc3060dad2
Hanumantha Reddy <109128221+hanumanthareddy9@users.noreply.github.com>
2022-07-23 09:57:57 +0530

    Add files via upload

---
+++ java-fsd/Assisted_Practice/src/Threadclass/MyThread.java
@@ -0,0 +1,14 @@
+package Threadclass;
+
+public class MyThread extends Thread
+{
+ 	public void run()
+ 	{
+  		System.out.println("concurrent thread started running..");
+}
+ 	public static void main( String args[] )
+ 	{
+  		MyThread mt = new  MyThread();
+  		mt.start();
+ 	}
+}
